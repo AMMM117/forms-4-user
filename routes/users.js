477240f34e03b5ebc6f3e3711accc5637637290b
anthonymartino <anthonymartino340@gmail.com>
2026-04-29 11:12:37 -0400

    usersfix/fin

--- routes/users.js
+++ routes/users.js
@@ -3,47 +3,68 @@ const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    res.send('User List');
+    res.render('list', { users });
   })
   .post((req, res) => {
     const firstName = req.body.firstName;
-    const isValid = firstName !== "";
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const isValid = firstName !== "" && lastName !== "" && gender !== "" && age !== "";
 
     if (isValid) {
-      console.log(`Adding user: ${firstName}`);
-      users.push({ firstName });
-      res.render('users/list', { users });
+      console.log(`Adding user: ${firstName} ${lastName}`);
+
+      users.push({
+        firstName: firstName,
+        lastName: lastName,
+        gender: gender,
+        age: age
+      });
+
+      res.redirect('/users/list');
     } else {
       console.log("Error adding user!");
-      res.render('users/new', { firstName: firstName });
+      res.render('new', {  
+        firstName: firstName,
+        lastName: lastName,
+        gender: gender,
+        age: age
+      });
     }
   });
 
 router.get('/list', (req, res) => {
-  res.render('users/list', { users });
+  res.render('list', { users }); 
 });
+
 router.get('/new', (req, res) => {
-  res.render('users/new', { firstName: "Test" });
+  res.render('new', {  
+    firstName: "",
+    lastName: "",
+    gender: "",
+    age: ""
+  });
 });
+
 router.route('/:id')
   .get((req, res) => {
-    console.log(req.user);
-    console.log('Getting user data');
-    res.send(`Getting User data for id: ${req.user['firstName']}`);
-  })
-  .delete((req, res) => {
-    res.send(`Deleting User data for id: ${req.params.id}`);
-  })
-  .put((req, res) => {
-    res.send(`Updating User data for id: ${req.params.id}`);
+    res.send(`
+      <h1>User Info</h1>
+      <p>First Name: ${req.user.firstName}</p>
+      <p>Last Name: ${req.user.lastName}</p>
+      <p>Gender: ${req.user.gender}</p>
+      <p>Age: ${req.user.age}</p>
+      <a href="/users/list">Back to User List</a>
+    `);
   });
+
 const users = [
-  { firstName: "George" },
-  { firstName: "Justyna" }
+  { firstName: "George", lastName: "Salayka", gender: "Male", age: 30 },
+  { firstName: "Justyna", lastName: "Example", gender: "Female", age: 25 }
 ];
 router.param('id', (req, res, next, id) => {
   req.user = users[id];
   next();
 });
-
 module.exports = router;
